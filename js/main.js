cbf5342ae0d01ae1c63f637b2753a3c39580b5f9
bekpulatovpro <jbekpulatov@yandex.ru>
2022-01-11 16:24:56 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var elSurveyForm = document.querySelector(".survey__form");
+var elSurveyInput = elSurveyForm.querySelector(".survey__input");
+var elSurveySelect = elSurveyForm.querySelector(".survey__select");
+var elSurveyResultBox = elSurveyForm.querySelector(".survey__result");
+
+var USD_to_SOUM = 10880;
+var EURO_to_SOUM = 12248.58;
+var RUBL_to_SOUM = 144.28;
+alert("Hello,this is updating machine and you may update USD,EURO and RUBL, Good luck for your job")
+
+elSurveyForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  var firstNumber = Number(elSurveyInput.value.trim());
+  var secondNumber = elSurveySelect.value.trim();
+
+  if (isNaN(firstNumber)) {
+    elConverterResultBox.textContent = "Enter number";
+    return;
+  }
+  if (secondNumber ==="USD") {
+    secondNumber = Math.floor(firstNumber*USD_to_SOUM);
+  }
+
+  if (secondNumber == "EURO") {
+    secondNumber = Math.floor(firstNumber*EURO_to_SOUM);
+  }
+
+  if (secondNumber == "RUBL") {
+    secondNumber =Math.floor(firstNumber*RUBL_to_SOUM) ;
+  }
+
+  var resultNumber = " Your budjet will stand "+ secondNumber + " SOUM";
+
+  elSurveyResultBox.textContent = resultNumber;
+});
